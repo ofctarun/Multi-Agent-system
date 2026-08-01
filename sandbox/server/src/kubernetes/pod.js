55dc59143d060483ff0ae086b57116fcbc3e02af
ofctarun <narayanashettitarun01@gmail.com>
2026-08-02 00:46:23 +0530

    fix(pod): update container images to use latest tags for consistency

--- sandbox/server/src/kubernetes/pod.js
+++ sandbox/server/src/kubernetes/pod.js
@@ -33,7 +33,7 @@ export async function createPod(sandboxId) {
             ,
             containers: [
                 {
-                    image: "template",
+                    image: "template:latest",
                     imagePullPolicy: "IfNotPresent",
                     name: "sandbox-container",
                     ports: [{ containerPort: 5173, name: "http" }],
@@ -49,7 +49,7 @@ export async function createPod(sandboxId) {
                     ]   
                 },
                 {
-                    image: "agent",
+                    image: "agent:latest",
                     imagePullPolicy: "IfNotPresent",
                     name: "agent-container",
                     ports: [{ containerPort: 3000, name: "http" }],
